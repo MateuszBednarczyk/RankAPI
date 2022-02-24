111aec54c239721ebaba0aeb7322bb6d98092593
MateuszBednarczyk <m.bednarczyk@zsek.olsztyn.pl>
2022-02-24 22:19:46 +0100

    working on getting highest score from selected game

--- src/main/java/com/example/rankapi/Controllers/RankController.java
+++ src/main/java/com/example/rankapi/Controllers/RankController.java
@@ -1,7 +1,21 @@
 package com.example.rankapi.Controllers;
 
+import com.example.rankapi.Entities.Rank;
+import com.example.rankapi.Repositories.RankRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RankController {
+
+    private RankRepository rankRepository;
+
+    @GetMapping("/rank/{gameTitle}")
+    public Rank getHighestRank(@PathVariable String gameTitle){
+
+        return rankRepository.findRankByGametitle(gameTitle);
+
+    }
+
 }
