c8d3e6c0f4c92eb5d7b032f8d6551ac65313263f
MateuszBednarczyk <m.bednarczyk@zsek.olsztyn.pl>
2022-02-24 01:19:01 +0100

    loggin doesnt work

--- src/main/java/com/example/rankapi/User/AppUserRepository.java
+++ src/main/java/com/example/rankapi/User/AppUserRepository.java
@@ -1,7 +1,9 @@
 package com.example.rankapi.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 
     AppUser findAppUserByUsername(String username);
