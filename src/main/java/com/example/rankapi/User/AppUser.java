28a0dbafb19891d2dd480e287aa69c271a1749ec
Mateusz Bednarczyk <91325367+MateuszBednarczyk@users.noreply.github.com>
2022-02-27 02:19:20 +0100

    Update AppUser.java

--- src/main/java/com/example/rankapi/User/AppUser.java
+++ src/main/java/com/example/rankapi/User/AppUser.java
@@ -32,7 +32,7 @@ public class AppUser implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
     @Override
