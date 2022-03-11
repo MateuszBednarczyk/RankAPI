7c51963547d2711f5c5b0a53507a89b219937448
MateuszBednarczyk <m.bednarczyk@zsek.olsztyn.pl>
2022-03-11 14:25:03 +0100

    it actually works

--- Clicker/src/main/java/com/example/clicker/ClickerController.java
+++ Clicker/src/main/java/com/example/clicker/ClickerController.java
@@ -3,7 +3,6 @@ package com.example.clicker;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @CrossOrigin(origins = "*")
---
+++ Snake/src/main/java/com/example/snake/SnakeController.java
@@ -0,0 +1,19 @@
+package com.example.snake;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@CrossOrigin(origins = "*")
+public class SnakeController {
+
+    @CrossOrigin
+    @GetMapping("/")
+    public String snake(){
+
+        return "snake.html";
+
+    }
+
+}
